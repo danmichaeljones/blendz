d2e07252d3869f569959887063f58b8f5e48fce7
Dan Jones <danmichaeljones@me.com>
2017-08-25 16:55:06 +0200

    Init skeleton structure

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='blendz',
+      version='0.0',
+      description='None',
+      author='None',
+      author_email='danmichaeljones@me.com',
+      license='None',
+      packages=['blendz'],
+      zip_safe=False)
