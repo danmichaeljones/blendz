e91e2759a31817cb2e656b7016e2bf18dada594c
Dan Jones desktop <danmichaeljones@me.com>
2017-09-18 17:27:00 +0100

    Partially editted base model

--- blendz/model/base.py
+++ blendz/model/base.py
@@ -37,111 +37,128 @@ class Base(ABC_meta):
             self.photometry = photometry
         self.num_templates = self.responses.templates.num_templates
         self.num_galaxies = self.photometry.num_galaxies
+        self.galaxy = None
         self.precalculateTemplatePriors()
 
     def precalculateTemplatePriors(self):
         self.template_priors = np.zeros(self.num_galaxies, self.num_templates)
-        for g in xrange(self.num_galaxies):
-            mag0 = self.ref_mag_data[g]
-            for t in xrange(self.num_templates):
-                tmpType = self.responses.templates.template_type(t)
-                template_priors[g, t] = self.lnPriorTemplate(tmpType, mag0)
+        for gal in self.photometry:
+            mag0 = gal.ref_mag_data
+            for T in xrange(self.num_templates):
+                tmpType = self.responses.templates.template_type(T)
+                template_priors[gal.index, T] = self.lnPriorTemplate(tmpType, mag0)
 
-    def negChiSq(self, c_model, gal):
-        out = -1. * np.sum((self.photometry.colour_data[gal] - c_model)**2 / self.photometry.colour_sigma_data[gal]**2)
+    def negChiSq(self, model_colour):
+        out = -1. * np.sum((self.galaxy.colour_data - model_colour)**2 / self.galaxy.colour_sigma**2)
         return out
 
-    def lnPosterior(self, redshifts, frac1, nblends, galaxy):
-        #Loop over all templates - discrete marginalisation
-        #All log probabilities so (multiply -> add)
-        # and (add -> logaddexp)
-        lnProb = -np.inf #Init at -ve inf (i.e., zero probability)
+    def lnPosterior(self, params):
+        nblends = (len(params)+1)/2
+        redshifts = params[:nblends]
+        fracs = params[nblends:]
+        if nblends>1:
+            #Impose a prior on redshifts being sorted
+            redshifts_okay = np.all(redshifts[1:] >= redshifts[:-1])
+            #Impose a prior on sum(frac) = 1
+            #fracs currently has length nblends-1
+            #so impose sum <= 1 and append final element after
+            fracs_okay = np.sum(fracs) <= 1.
+            params_okay = redshifts_okay * fracs_okay
+        else:
+            #Single object case, so no need to check sorting/sum conditions
+            params_okay = True
 
-        #Should move things out of the loops a bit to speed up execution.
-        #e.g., for 8 templates, each self.modelFlux line is called 64 times each
-        #instead of the 8 required. Should calculate what's needed then cache rather
-        #than wasting calculations.
+        if not params_okay:
+            return -np.inf
+        else:
+            #Get final frac by imposing sum-to-one condition
+            fracs = np.append(fracs, 1.-np.sum(fracs))
 
-        #Precalculate the redshift priors
-        redshift_priors = np.zeros(len(redshifts))
+            #Precalculate all quantities we'll need in the template loop
+            redshift_priors = np.zeros((nblends, self.num_templates))
+            model_fluxes = np.zeros((nblends, self.num_templates))
+            for iz, Z in enumerate(redshifts):
+                for T in xrange(self.num_templates):
+                    tmpType = self.responses.templates.template_type(T)
+                    redshift_priors[iz, T] = self.lnPriorRedshift(Z, tmpType)
+                    model_fluxes[iz, T] = self.modelFlux(Z, T)
+            correlation_function = self.correlationFunction(redshifts)
+            frac_prior = self.lnPriorFrac(fracs)
 
-        #At each iteration template_combo is a tuple of (T_1, T_2... T_nblends)
-        for template_combo in itr.product(*itr.repeat(xrange(self.num_templates), nblends)):
-            #Inside here, instead of fixing each line to have tmp += f(t1) + f(t2)
-            #which is for 2-blends, can can increment tmp with f(ti) for ti in x from above
-            #The redshift sorting should be done as checking ascending(zArray)
-            #Maybe would make sense to add a z-positive prior in here rather
-            #than relying on the prior for easier usage.
-            #This whole idea might require frac be an array nb long. This could be done
-            #by moving to a Dirichlet prior on frac so that we can enforce sum-to-one.
-            #The posterior will have to be changed to accept parameters as a 1d array, theta.
-            #Since we only need reshifts and frac, each are nb long, and theta is 2*nb long.
+            #Loop over all templates - discrete marginalisation
+            #All log probabilities so (multiply -> add)
+            # and (add -> logaddexp)
+            lnProb = -np.inf #Init at -ve inf (i.e., zero probability)
 
-            #Template prior
-            #Assume independance between template of components so
-            #P(T1, T2 | m0) = P(T1 | m0) * P(T2 | m0) * ...
-            for T in template_combo:
-                tmp += self.template_priors(galaxy, T)
+            #At each iteration template_combo is a tuple of (T_1, T_2... T_nblends)
+            for template_combo in itr.product(*itr.repeat(xrange(self.num_templates), nblends)):
+                #Add a loop over nblends components here probably, should be
+                #able to combine the loops below into this one
 
-            #Add redshift prior - check z1 <= z2 <= ... for sorting
-            if redshift1 <= redshift2:
+                #Template prior
+                #Assume independance between template of components so
+                #P(T1, T2 | m0) = P(T1 | m0) * P(T2 | m0) * ...
+                for T in template_combo:
+                    tmp += self.template_priors[self.galaxy.index, T]
+
+                #Add redshift prior
                 #Product of P(z1) and P(z2), with extra correlation from xi(r[z1, z2])
-                tmp = tmp + self.lnPriorRedshift(redshift1, t1)
-                tmp = tmp + self.lnPriorRedshift(redshift2, t2)
-                tmp = tmp + (1. + self.correlationFunction(redshift1, redshift2))
-            else:
-                tmp = tmp -np.inf
+                for b in xrange(nblends): #this could go into the T loop...
+                    tmp += redshift_priors[redshifts[b], template_combo[b]]
 
-            #Calculate the model flux at these parameters
-            #Two components - total model flux is the sum
-            #This is *actual* model flux not the log of it, so
-            #just sum them together
-            f_model_unscaled1 = self.modelFlux(redshift1, t1) * frac1
-            f_model_unscaled2 = self.modelFlux(redshift2, t2) * (1. - frac1)
-            f_model_unscaled = f_model_unscaled1 + f_model_unscaled2
-            f_model = f_model_unscaled * self.getModelScaling(f_model_unscaled)
+                #Extra correlation between redshifts over independent
+                tmp += (1. + correlation_function)
 
-            #Add flux fraction prior
-            tmp = tmp + self.lnPriorFrac(frac1)
+                #Calculate the model flux at these parameters
+                #Two components - total model flux is the sum
+                #This is *actual* model flux not the log of it, so
+                #just sum them together
+                f_model_unscaled1 = self.modelFlux(redshift1, t1) * frac1
+                f_model_unscaled2 = self.modelFlux(redshift2, t2) * (1. - frac1)
+                f_model_unscaled = f_model_unscaled1 + f_model_unscaled2
+                f_model = f_model_unscaled * self.getModelScaling(f_model_unscaled)
 
-            #Add likelihood
-            #lnProbTemplate[ti] += self.negChiSq(f_model)
-            tmp = tmp + self.negChiSq(f_model)
+                #Add flux fraction prior
+                tmp = tmp + self.lnPriorFrac(frac1)
 
-            #logaddexp contribution from this template to marginalise
-            lnProb = np.logaddexp(lnProb, tmp)
+                #Add likelihood
+                #lnProbTemplate[ti] += self.negChiSq(f_model)
+                tmp = tmp + self.negChiSq(f_model)
 
-    #Catch nans from two negative redshifts
-    if np.isfinite(lnProb):
-        return lnProb
-    else:
-        return -np.inf
+                #logaddexp contribution from this template to marginalise
+                lnProb = np.logaddexp(lnProb, tmp)
 
-    def sample(self, nblends=None, gal=None):
+        #Catch nans from two negative redshifts
+        if np.isfinite(lnProb):
+            return lnProb
+        else:
+            return -np.inf
+
+    def sample(self, nblends, gal=None):
         #TODO: Implement this function.
         '''
-        nblends should be a list so that multiple
+        nblends should be int, or could be a list so that multiple
         different nb's can be done and compared for evidence etc.
-        If none, some default maybe? Maybe demand it's set..
 
         galaxy should be an int choosing which galaxy of all in photometry
         to estimate the redshift of. If none, do all of them.
         '''
+        #self.galaxy = self.photometry[gal]
         pass
 
     @abc.abstractmethod
-    def correlationFunction(self, redshift1, redshift2):
+    def correlationFunction(self, redshifts):
         pass
 
     @abc.abstractmethod
-    def lnTemplatePrior(self, templateIndex):
+    def lnTemplatePrior(self, templateType):
         #Would make sense to pass type, not index, to hide the index->type check
         pass
 
     @abc.abstractmethod
-    def lnRedshift(self, redshift, templateIndex):
+    def lnRedshift(self, redshift, templateType):
         pass
 
     @abc.abstractmethod
-    def lnFracPrior(self, frac):
+    def lnFracPrior(self, fracs):
         pass
