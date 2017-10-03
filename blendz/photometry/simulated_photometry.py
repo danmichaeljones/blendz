c630d87ab545fd917673197cdade208f87be681f
Dan Jones desktop <danmichaeljones@me.com>
2017-10-03 16:44:40 +0100

    Save fraction from simulations

--- blendz/photometry/simulated_photometry.py
+++ blendz/photometry/simulated_photometry.py
@@ -20,18 +20,21 @@ class SimulatedPhotometry(PhotometryBase):
         else:
             self.responses = responses
 
-        self.simulateGalaxies(self.num_sims, self.num_components, self.max_redshift, self.max_scale, self.max_err_frac)
+        self.simulateRandomGalaxies(self.num_sims, self.num_components, self.max_redshift, self.max_scale, self.max_err_frac)
 
     def generateBlendMagnitude(self, num_components, redshifts, scales, template_indices, err_frac):
         true_flux = np.zeros(self.responses.filters.num_filters)
         for c in xrange(num_components):
             true_flux += self.responses(template_indices[c], None, redshifts[c]) * scales[c]
+        fracs = np.zeros(num_components)
+        for c in xrange(num_components):
+            fracs[c] = ((self.responses(template_indices[c], None, redshifts[c]) * scales[c]) / true_flux)[_config.ref_band]
         rand_err  = (np.random.rand(self.responses.filters.num_filters) * (true_flux * err_frac * 2)) - (true_flux * err_frac)
         obs_flux = true_flux + rand_err
         flux_err = true_flux * err_frac
         obs_mag = np.log10(obs_flux) / (-0.4)
         mag_err = np.log10((flux_err/obs_flux)+1.) / (-0.4)
-        return obs_mag, mag_err
+        return obs_mag, mag_err, fracs
 
     def randomBlend(self, num_components, max_redshift, max_scale, max_err_frac):
         sim_redshift = np.sort(self.rstate.rand(num_components) * max_redshift)
@@ -39,17 +42,37 @@ class SimulatedPhotometry(PhotometryBase):
         sim_template = self.rstate.randint(0, self.responses.templates.num_templates, num_components)
         sim_err_frac = self.rstate.rand() * max_err_frac
 
+        obs_mag, mag_err, fracs = self.generateBlendMagnitude(num_components, sim_redshift, sim_scale, sim_template, sim_err_frac)
+
         truth = {}
         truth['num_components'] = num_components
         for c in xrange(num_components):
-            truth[c] = {'redshift': sim_redshift[c], 'scale': sim_scale[c], 'template': sim_template[c]}
+            truth[c] = {'redshift': sim_redshift[c], 'scale': sim_scale[c],
+            'template': sim_template[c], 'fraction': fracs[c]}
 
-        obs_mag, mag_err = self.generateBlendMagnitude(num_components, sim_redshift, sim_scale, sim_template, sim_err_frac)
         return obs_mag, mag_err, truth
 
-    def simulateGalaxies(self, num_sims, num_components, max_redshift, max_scale, max_err_frac):
+    def simulateRandomGalaxies(self, num_sims, num_components, max_redshift, max_scale, max_err_frac):
         for g in xrange(num_sims):
             mag_data, mag_sigma, truth = self.randomBlend(num_components, max_redshift, max_scale, max_err_frac)
             new_galaxy = Galaxy(mag_data, mag_sigma, _config.ref_band, self.zero_point_frac, g)
+            new_galaxy.truth = truth
+            self.galaxies.append(new_galaxy)
+
+    def simulateGalaxies(self, redshifts, scales, templates, err_frac):
+        for g in xrange(len(redshifts)):
+            num_components = len(redshifts[g])
+            obs_mag, mag_err, fracs = self.generateBlendMagnitude(
+                                        num_components, redshifts[g], scales[g],
+                                        templates[g], err_frac)
+            truth = {}
+            truth['num_components'] = num_components
+            for c in xrange(num_components):
+                truth[c] = {'redshift': redshifts[g][c],
+                            'scale': scales[g][c],
+                            'fraction': fracs[c],
+                            'template': templates[g][c]}
+            new_galaxy = Galaxy(obs_mag, mag_err, _config.ref_band,
+                                self.zero_point_frac, g)
             new_galaxy.truth = truth
             self.galaxies.append(new_galaxy)
