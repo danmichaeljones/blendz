edb586e6e1bf281e26331c9348ced4c64f8a1457
Dan Jones desktop <danmichaeljones@me.com>
2017-10-03 10:34:45 +0100

    Add ref-mag parameter to likelihood-only priors

--- blendz/model/likelihood_only.py
+++ blendz/model/likelihood_only.py
@@ -3,10 +3,10 @@ from blendz.model import Base
 
 class LikelihoodOnly(Base):
 
-    def lnTemplatePrior(self, template_type):
+    def lnTemplatePrior(self, template_type, component_ref_mag):
         return 0.
 
-    def lnRedshiftPrior(self, redshift, template_type):
+    def lnRedshiftPrior(self, redshift, template_type, component_ref_mag):
         return 0.
 
     def correlationFunction(self, redshifts):
