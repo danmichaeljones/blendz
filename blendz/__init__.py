9e5facedfe55c0c4a8b86a1bcbef7b7802136d79
Dan Jones desktop <danmichaeljones@me.com>
2018-01-18 16:51:02 +0000

    Alias Configuration

--- blendz/__init__.py
+++ blendz/__init__.py
@@ -3,5 +3,6 @@ import os
 PACKAGE_PATH = os.path.dirname(__file__)
 RESOURCE_PATH = os.path.abspath(os.path.join(PACKAGE_PATH, 'resources'))
 
+from .config import Configuration
 from .photoz import Photoz
 from blendz.model import BPZ, LikelihoodOnly
