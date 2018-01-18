6ceab410af88435fe1074ce840942db29265aced
Dan Jones desktop <danmichaeljones@me.com>
2018-01-18 14:08:50 +0000

    Removed old fraction method

--- blendz/__init__.py
+++ blendz/__init__.py
@@ -3,6 +3,5 @@ import os
 PACKAGE_PATH = os.path.dirname(__file__)
 RESOURCE_PATH = os.path.abspath(os.path.join(PACKAGE_PATH, 'resources'))
 
-from .photoz import Photoz
 from .photoz_mag import PhotozMag
 from blendz.model import BPZ, LikelihoodOnly
