860a6d965c8e379aadea4275109a0e7581bfe2b2
Dan Jones desktop <danmichaeljones@me.com>
2018-02-16 17:13:21 +0000

    Changed no observe/detect check to allow tolerance in float comparison

--- blendz/photometry/galaxy.py
+++ blendz/photometry/galaxy.py
@@ -20,29 +20,28 @@ class Galaxy(object):
         self.flux_data = 10.**(-0.4*self.mag_data)
         self.flux_sigma = (10.**(0.4*self.mag_sigma)-1.)*self.flux_data
 
-        #If the magnitude is -99, the galaxy is not observed in that band,
-        #    so set flux to zero and error to very high (1e108)
-        noObs = np.where(self.mag_data==self.config.no_observe_value)[0]
+        # Galaxy not OBSERVED - set flux to zero and error to very high (1e108)
+        noObs = np.where(np.isclose(self.mag_data, self.config.no_observe_value))[0]
         for no in noObs:
             self.flux_data[no] = 0.
             self.flux_sigma[no] = 1e108
 
-        #If the magnitude is 99, the galaxy is not detected in that band,
-        #    so set flux to zero, error to abs(error)
-        noDet = np.where(self.mag_data==self.config.no_detect_value)[0]
+        # Galaxy not DETECTED - set flux to zero, error to abs(error)
+        noDet = np.where(np.isclose(self.mag_data, self.config.no_detect_value))[0]
         for nd in noDet:
             self.flux_data[nd] = 0.
             self.flux_sigma[nd] = 10.**(-0.4*self.mag_sigma[nd])
 
-        #Array of indices for bands where galaxy is neither not-observed nor not-detected
+        #Array of bools for bands where galaxy is neither not-observed nor not-detected
         # i.e., the galaxy IS seen in this band
-        seen = [(self.mag_data!=self.config.no_observe_value) * (self.mag_data!=self.config.no_detect_value)]
-        notSeen = [~((self.mag_data!=self.config.no_observe_value) * (self.mag_data!=self.config.no_detect_value))]
+        notSeen = np.array([((b in noObs) or (b in noDet)) for b in range(len(self.mag_data))])
+        seen = np.array([not b for b in notSeen])
+
         #Add the zero point errors in
         #First, handle the observed objects
-        self.flux_sigma = np.where(seen, np.sqrt(self.flux_sigma*self.flux_sigma+(self.zero_point_frac*self.flux_data)**2.), self.flux_sigma)[0]
+        self.flux_sigma = np.where(seen, np.sqrt(self.flux_sigma*self.flux_sigma+(self.zero_point_frac*self.flux_data)**2.), self.flux_sigma)
         #Handle objects not seen differently to observed objects
-        self.flux_sigma = np.where(notSeen, np.sqrt(self.flux_sigma*self.flux_sigma+(self.zero_point_frac*(self.flux_sigma/2.))**2), self.flux_sigma)[0]
+        self.flux_sigma = np.where(notSeen, np.sqrt(self.flux_sigma*self.flux_sigma+(self.zero_point_frac*(self.flux_sigma/2.))**2), self.flux_sigma)
 
         #Create attribute for the flux in the reference band
         self.ref_flux_data = self.flux_data[self.config.ref_band]
