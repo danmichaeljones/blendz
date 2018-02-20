Fixed numerical errors from assumed magnitude range

blendz/model/bpz.py
@@ -38,14 +38,14 @@ class BPZ(ModelBase):
         if template_type in ['early', 'late']:
             Nt = self.responses.templates.numType(template_type)
             coeff = np.log(self.prior_params_dict['f_t'][template_type] / Nt)
-            expon = self.prior_params_dict['k_t'][template_type] * (component_ref_mag - 20.)
+            expon = self.prior_params_dict['k_t'][template_type] * (component_ref_mag - self.config.ref_mag_lo)
             out = coeff - expon
         elif template_type == 'irr':
             Nte = self.responses.templates.numType('early')
             Ntl = self.responses.templates.numType('late')
             Nti = self.responses.templates.numType('irr')
-            expone = self.prior_params_dict['k_t']['early'] * (component_ref_mag - 20.)
-            exponl = self.prior_params_dict['k_t']['late'] * (component_ref_mag - 20.)
+            expone = self.prior_params_dict['k_t']['early'] * (component_ref_mag - self.config.ref_mag_lo)
+            exponl = self.prior_params_dict['k_t']['late'] * (component_ref_mag - self.config.ref_mag_lo)
             early = self.prior_params_dict['f_t']['early'] * np.exp(-expone)
             late = self.prior_params_dict['f_t']['late'] * np.exp(-exponl)
             out = np.log(1. - early - late) - np.log(Nti)
@@ -61,14 +61,18 @@ class BPZ(ModelBase):
                 first = -np.inf
             else:
                 first = (self.prior_params_dict['alpha_t'][template_type] * np.log(redshift))
-            second = self.prior_params_dict['z_0t'][template_type] + (self.prior_params_dict['k_mt'][template_type] * (component_ref_mag - 20.))
+            second = self.prior_params_dict['z_0t'][template_type] + (self.prior_params_dict['k_mt'][template_type] * (component_ref_mag - self.config.ref_mag_lo))
             out = first - (redshift / second)**self.prior_params_dict['alpha_t'][template_type]
         except KeyError:
             raise ValueError('The BPZ priors are only defined for templates of \
                               types "early", "late" and "irr", but the redshift \
                               prior was called with type ' + template_type)
         if norm:
-            return out + self.redshift_prior_norm[template_type](component_ref_mag)
+            try:
+                out = out + self.redshift_prior_norm[template_type](component_ref_mag)
+            except ValueError:
+                raise ValueError('Magnitude = {} is outside of prior-precalculation range. Check your configuration ref-mag limits cover your input magnitudes.'.format(component_ref_mag))
+            return out
         else:
             return out
 