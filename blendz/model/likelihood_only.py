2f1215e641ed1f7e194f0501361a5b24cf5cfaf4
Dan Jones desktop <danmichaeljones@me.com>
2018-05-08 16:53:31 +0100

    Changed likelihood only model to use total prior function

--- blendz/model/likelihood_only.py
+++ blendz/model/likelihood_only.py
@@ -3,15 +3,5 @@ import numpy as np
 from blendz.model import ModelBase
 
 class LikelihoodOnly(ModelBase):
-
-    def lnTemplatePrior(self, template_type, component_ref_mag):
-        return 0.
-
-    def lnRedshiftPrior(self, redshift, template_type, component_ref_mag):
-        return 0.
-
-    def correlationFunction(self, redshifts):
-        return 0.
-
-    def lnMagnitudePrior(self, magnitude):
+    def lnPrior(self, redshift, magnitude):
         return 0.
